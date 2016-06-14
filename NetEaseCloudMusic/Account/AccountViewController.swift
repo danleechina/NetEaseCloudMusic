20c5946c5ace2dd28ddb66dcfab0fcade4d67cba
lizhengda <zhengda_li@kingdee.com>
2016-06-14 20:36:41 +0800

    添加基本的vc

---
+++ NetEaseCloudMusic/Account/AccountViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AccountViewController.swift
+//  NetEaseCloudMusic
+//
+//  Created by Ampire_Dan on 16/6/14.
+//  Copyright © 2016年 Ampire_Dan. All rights reserved.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
+class AccountHeadView: UIView {
+    
+    init(frame: CGRect) {
+        
+    }
+}
---
+++ NetEaseCloudMusic/FindMusic/SoundHoundViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SoundHoundViewController.swift
+//  NetEaseCloudMusic
+//
+//  Created by Ampire_Dan on 16/6/14.
+//  Copyright © 2016年 Ampire_Dan. All rights reserved.
+//
+
+import UIKit
+
+class SoundHoundViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
