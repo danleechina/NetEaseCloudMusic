6dfae3666843aff78f92cdcabb612c202a72134d
danleechina <349604757@qq.com>
2016-06-16 16:12:32 +0800

    添加Pod

--- NetEaseCloudMusic/Account/AccountViewController.swift
+++ NetEaseCloudMusic/Account/AccountViewController.swift
@@ -35,7 +35,13 @@ class AccountViewController: UIViewController {
 }
 
 extension AccountViewController:UITableViewDelegate, UITableViewDataSource {
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
 }
 
 
