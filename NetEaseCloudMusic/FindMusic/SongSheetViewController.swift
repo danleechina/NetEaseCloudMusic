802c36f098bef9de338cdbdf11c7a292395fb450
danleechina <349604757@qq.com>
2016-07-09 17:48:32 +0800

    添加基本代码

--- NetEaseCloudMusic/FindMusic/SongSheetViewController.swift
+++ NetEaseCloudMusic/FindMusic/SongSheetViewController.swift
@@ -9,27 +9,76 @@
 import UIKit
 
 class SongSheetViewController: UIViewController {
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView.init(frame: self.view.bounds, collectionViewLayout: self.collectionViewFlowLayout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = UIColor.clearColor()
+        collectionView.registerClass(SongSheetCollectionViewCell.self, forCellWithReuseIdentifier:SongSheetCollectionViewCell.identifier)
+        collectionView.registerClass(UICollectionReusableView.self, forSupplementaryViewOfKind:UICollectionElementKindSectionHeader, withReuseIdentifier:SongSheetCollectionViewCell.identifier)
+        return collectionView
+    }()
+    
+    private lazy var collectionViewFlowLayout: UICollectionViewFlowLayout = {
+       let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .Vertical
+        layout.headerReferenceSize = CGSizeMake(CGRectGetWidth(self.view.frame), 100)
+        layout.itemSize = CGSizeMake(140, 120)
+        
+        return layout
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.whiteColor()
+        view.addSubview(self.collectionView)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+
+extension SongSheetViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    // MARK: - DataSource
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(SongSheetCollectionViewCell.identifier, forIndexPath: indexPath) as! SongSheetCollectionViewCell
+        cell.backgroundColor = UIColor.blueColor()
+        return cell
     }
-    */
+    
+    func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
+        let view = collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: SongSheetCollectionViewCell.identifier, forIndexPath: indexPath)
+        view.backgroundColor = UIColor.redColor()
+        return view
+    }
+    
+    // MARK: - Delegate
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+}
 
+
+
+
+class SongSheetCollectionViewCell: UICollectionViewCell {
+    static let identifier = "SongSheetCollectionViewCell"
+    private var imageView = UIImageView()
+    private var titleLabel = UILabel()
+    private var authorLabel = UILabel()
+    private var starImageView = UIImageView()
+    private var subscribeLabel = UILabel()
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+    }
+    
 }
