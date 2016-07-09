7bcf8dd537c6502c7194951dbdc77435c53a7629
danleechina <349604757@qq.com>
2016-07-09 14:16:45 +0800

    去掉测试代码

--- NetEaseCloudMusic/AppDelegate.swift
+++ NetEaseCloudMusic/AppDelegate.swift
@@ -16,9 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
-        let network = NetworkMusicApi()
-        network.login("349604757@qq.com", password: "Lee1234567890")
         return true
     }
 
