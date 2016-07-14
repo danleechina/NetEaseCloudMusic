02ec38ba6064c175e428867764318f7cab3f7cb7
danleechina <349604757@qq.com>
2016-07-14 20:58:35 +0800

    add some outlet to the playsongvc

--- NetEaseCloudMusic/MyMusic/PlaySongViewController.swift
+++ NetEaseCloudMusic/MyMusic/PlaySongViewController.swift
@@ -9,7 +9,94 @@
 import UIKit
 
 class PlaySongViewController: UIViewController {
-
+    //I made a mistake, I should let the type be UIButton not UIImageView, but I am lazy to change. So be it
+    
+    @IBOutlet weak var needleImageView: UIImageView! {
+        didSet {
+            
+        }
+    }
+    
+    @IBOutlet weak var themePicImageView: UIImageView!{
+        didSet {
+            
+        }
+    }
+    
+    @IBOutlet weak var loveImageView: UIImageView!{
+        didSet {
+            
+        }
+    }
+    
+    @IBOutlet weak var downloadImageView: UIImageView!{
+        didSet {
+            
+        }
+    }
+    
+    @IBOutlet weak var commentImageView: UIImageView!{
+        didSet {
+            
+        }
+    }
+    
+    @IBOutlet weak var settingImageView: UIImageView!{
+        didSet {
+            
+        }
+    }
+    
+    @IBOutlet weak var timePointLabel: UILabel!{
+        didSet {
+            
+        }
+    }
+    
+    @IBOutlet weak var totalTimeLabel: UILabel!{
+        didSet {
+            
+        }
+    }
+    
+    @IBOutlet weak var songProgressView: UIProgressView!{
+        didSet {
+            
+        }
+    }
+    
+    @IBOutlet weak var playModeImageView: UIImageView!{
+        didSet {
+            
+        }
+    }
+    
+    @IBOutlet weak var lastSongImageView: UIImageView!{
+        didSet {
+            
+        }
+    }
+    
+    @IBOutlet weak var playImageView: UIImageView!{
+        didSet {
+            
+        }
+    }
+    
+    @IBOutlet weak var nextImageView: UIImageView!{
+        didSet {
+            
+        }
+    }
+    
+    @IBOutlet weak var totalSettingImageView: UIImageView!{
+        didSet {
+            
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
