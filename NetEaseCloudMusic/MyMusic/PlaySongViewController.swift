b5161065bbd075d3c6e93a0b12de8781d00d64d7
danleechina <349604757@qq.com>
2016-07-18 20:04:46 +0800

    添加一点点击事件

--- NetEaseCloudMusic/MyMusic/PlaySongViewController.swift
+++ NetEaseCloudMusic/MyMusic/PlaySongViewController.swift
@@ -13,7 +13,6 @@ class PlaySongViewController: UIViewController {
     
     @IBOutlet weak var needleImageView: UIImageView! {
         didSet {
-            
         }
     }
     
@@ -59,11 +58,13 @@ class PlaySongViewController: UIViewController {
         }
     }
     
-    @IBOutlet weak var songProgressView: UIProgressView!{
+    @IBOutlet weak var currentLocationSlider: UISlider! {
         didSet {
-            
+            currentLocationSlider.setThumbImage(UIImage.init(named: "cm2_fm_playbar_btn"), forState: .Normal)
+            currentLocationSlider.minimumTrackTintColor = FixedValue.mainRedColor
         }
     }
+    
     
     @IBOutlet weak var playModeImageView: UIImageView!{
         didSet {
@@ -79,9 +80,12 @@ class PlaySongViewController: UIViewController {
     
     @IBOutlet weak var playImageView: UIImageView!{
         didSet {
-            
+            let tapGest = UITapGestureRecognizer.init(target: self, action: #selector(tapPlayImage))
+            tapGest.numberOfTapsRequired = 1
+            playImageView.addGestureRecognizer(tapGest)
         }
     }
+    
     
     @IBOutlet weak var nextImageView: UIImageView!{
         didSet {
@@ -101,18 +105,53 @@ class PlaySongViewController: UIViewController {
         }
     }
     
+    var isPlaying = false
+    
+    func tapPlayImage() -> Void {
+        var angle:CGFloat = 0
+        if isPlaying {
+            playImageView.image = UIImage.init(named: "cm2_fm_btn_pause")
+            angle = -CGFloat(M_PI/360 * 80)
+        } else {
+            playImageView.image = UIImage.init(named: "cm2_fm_btn_play")
+            angle = CGFloat(0)
+        }
+        self.setAnchorPoint(CGPointMake(0.1, 0.1), forView: self.needleImageView)
+        UIView .animateWithDuration(0.2) {
+            self.needleImageView.transform = CGAffineTransformMakeRotation(angle)
+        }
+        isPlaying = !isPlaying
+    }
+    
+    func setAnchorPoint(anchorPoint: CGPoint, forView view: UIView) {
+        var newPoint = CGPointMake(view.bounds.size.width * anchorPoint.x, view.bounds.size.height * anchorPoint.y)
+        var oldPoint = CGPointMake(view.bounds.size.width * view.layer.anchorPoint.x, view.bounds.size.height * view.layer.anchorPoint.y)
+        
+        newPoint = CGPointApplyAffineTransform(newPoint, view.transform)
+        oldPoint = CGPointApplyAffineTransform(oldPoint, view.transform)
+        
+        var position = view.layer.position
+        position.x -= oldPoint.x
+        position.x += newPoint.x
+        
+        position.y -= oldPoint.y
+        position.y += newPoint.y
+        
+        view.layer.position = position
+        view.layer.anchorPoint = anchorPoint
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        isPlaying = true
     }
     
-    override func viewDidLayoutSubviews() {
-        
-        var frame = view.frame
-        frame.origin.y += 64
-        frame.size.height -= 64
-        view.frame = frame
-    }
+//    override func viewDidLayoutSubviews() {
+//        
+//        var frame = view.frame
+//        frame.origin.y += 64
+//        frame.size.height -= 64
+//        view.frame = frame
+//    }
 
 }
