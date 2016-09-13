cc3dcf20c2173d6e1334d0b7f0000ae86be9d7a3
Zhengda <349604757@qq.com>
2016-09-13 20:25:38 +0800

    add common navigation bar

---
+++ NetEaseCloudMusic/Common/BaseNavigationBar.swift
@@ -0,0 +1,18 @@
+//
+//  BaseNavigationBar.swift
+//  NetEaseCloudMusic
+//
+//  Created by ios on 16/9/13.
+//  Copyright © 2016年 Ampire_Dan. All rights reserved.
+//
+
+import UIKit
+
+class BaseNavigationBar: UIView {
+    var titleView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    
+}
