3db915d185d06a02b0fb751110215fc14b5beb7f
Zhengda <349604757@qq.com>
2016-12-31 21:02:34 +0800

    SliderViewController using uicollectionview to replace uitableview for navigation view

--- NetEaseCloudMusic/Common/SliderViewController.swift
+++ NetEaseCloudMusic/Common/SliderViewController.swift
@@ -8,12 +8,23 @@
 
 import UIKit
 
+class DefaultStyleAttribute {
+    static let indicatorViewColor = UIColor.green
+    static let indicatorViewHeight: CGFloat = 4
+    static let indicatorViewWidthDiff: CGFloat = 4
+    
+    static let cellTitleLabelFont = UIFont.systemFont(ofSize: 15)
+}
+
 class SliderViewController: UIViewController, UIScrollViewDelegate {
     var contentViewControllers = [UIViewController]()
     var currentIndex: Int = 0
+    var titleTexts = [String]()
+    
     
     fileprivate let navigationView = InternalNavigationView()
     fileprivate let contentView = InternalContentView()
+    fileprivate let calWidthLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,18 +36,24 @@ class SliderViewController: UIViewController, UIScrollViewDelegate {
         contentView.contentSize = CGSize(width: view.frame.width * CGFloat(contentViewControllers.count), height: view.frame.height)
         contentView.isPagingEnabled = true
         contentView.bounces = false
-        contentView.backgroundColor = UIColor.purple
         contentView.showsVerticalScrollIndicator = false
         contentView.showsHorizontalScrollIndicator = false
         
-        navigationView.titleTexts = ["this one", "this two", "this three"]
-        navigationView.frame = CGRect(x: 0, y: 100, width: view.frame.width, height: 44)
-        navigationView.backgroundColor = UIColor.brown
+        navigationView.titleTexts = titleTexts
+        navigationView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 44)
+        navigationView.action = navigationViewSelectedAction
         
+        customIndicatorViewWith()
         add(viewFromIndex: currentIndex)
     }
     
+    func navigationViewSelectedAction(selectedIndex: Int) {
+        goTo(viewAtIndex: selectedIndex)
+    }
+    
     func add(viewFromIndex index: Int) {
+        guard index >= 0 && index < self.contentViewControllers.count else { return }
+        
         let vc = self.contentViewControllers[index]
         if vc.view.superview == nil {
             let offsetX = self.contentView.bounds.width * CGFloat(index)
@@ -52,11 +69,46 @@ class SliderViewController: UIViewController, UIScrollViewDelegate {
         currentIndex = index
     }
     
+    func customIndicatorViewWith() {
+        let scrollView = contentView
+        var nextIndex = currentIndex
+        let panGest = scrollView.panGestureRecognizer
+        if panGest.velocity(in: scrollView.superview).x > 0 {
+            // left is about to appear
+            nextIndex -= 1
+        } else {
+            // right is about to appear
+            nextIndex += 1
+        }
+        if nextIndex < 0 || nextIndex >= contentViewControllers.count {
+            return
+        }
+        
+        let offsetX = scrollView.contentOffset.x / scrollView.contentSize.width * navigationView.frame.width
+        calWidthLabel.font = DefaultStyleAttribute.cellTitleLabelFont
+        calWidthLabel.text = navigationView.titleTexts[nextIndex]
+        calWidthLabel.sizeToFit()
+        let resWidth = calWidthLabel.frame.width + DefaultStyleAttribute.indicatorViewWidthDiff
+        let startWidth = navigationView.indicatorView.frame.width
+        let complete = abs((scrollView.contentOffset.x - scrollView.contentSize.width * CGFloat(self.currentIndex)) / scrollView.frame.width)
+        let idWidth = startWidth - resWidth * complete
+        navigationView.configIndicatorViewSize(offsetX: offsetX + scrollView.frame.width/CGFloat(titleTexts.count)/2 - idWidth/2, width: idWidth)
+    }
+    
     // - MARK: Scroll View Delegate
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let nextIndex = ceilf(Float(scrollView.contentOffset.x / scrollView.bounds.width))
+        var nextIndex = currentIndex
+        let panGest = scrollView.panGestureRecognizer
+        if panGest.velocity(in: scrollView.superview).x > 0 {
+            // left is about to appear
+            nextIndex -= 1
+        } else {
+            // right is about to appear
+            nextIndex += 1
+        }
         add(viewFromIndex: Int(nextIndex))
+        customIndicatorViewWith()
     }
     
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
@@ -69,6 +121,134 @@ class SliderViewController: UIViewController, UIScrollViewDelegate {
             let nextIndex = Int(scrollView.contentOffset.x / scrollView.bounds.width + 0.5)
             goTo(viewAtIndex: nextIndex)
         }
+    }
+}
+
+class InternalNavigationView: UIView,UIScrollViewDelegate, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    var action: ((_ selectedIndex: Int) -> ())?
+    var titleTexts = [String]() {
+        didSet {
+            if titleTexts.count > 0 {
+                configIndicatorViewSize(offsetX: 0, width: frame.width/CGFloat(titleTexts.count))
+            }
+        }
+    }
+    
+    fileprivate lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        let collectionView = UICollectionView.init(frame: self.bounds, collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = UIColor.clear
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.register(InternalNavigationCell.self, forCellWithReuseIdentifier: InternalNavigationCell.identifier)
+        
+        return collectionView
+    }()
+    
+    fileprivate lazy var indicatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = DefaultStyleAttribute.indicatorViewColor
+        return view
+    }()
+    
+    fileprivate func customInit() {
+        addSubview(collectionView)
+        addSubview(indicatorView)
+    }
+    
+    override var frame: CGRect {
+        didSet {
+            collectionView.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.height)
+            if titleTexts.count > 0 {
+                configIndicatorViewSize(offsetX: 0, width: frame.width/CGFloat(titleTexts.count))
+            }
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        customInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        customInit()
+    }
+    
+    func configIndicatorViewSize(offsetX: CGFloat, width: CGFloat) {
+        indicatorView.frame = CGRect(x: offsetX, y: frame.height - DefaultStyleAttribute.indicatorViewHeight, width: width, height: DefaultStyleAttribute.indicatorViewHeight)
+    }
+    
+    // MARK: - Collection View Delegate
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        action?(indexPath.row)
+    }
+    
+    // MARK: - Collection View Datasource
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return titleTexts.count
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: InternalNavigationCell.identifier, for: indexPath) as! InternalNavigationCell
+        cell.titleLabel.text = titleTexts[indexPath.row]
+        return cell
+    }
+    
+    // MARK: - Collection View Layout Delegate
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width/CGFloat(titleTexts.count), height: collectionView.frame.height)
+    }
+}
+
+class InternalNavigationCell: UICollectionViewCell {
+    static let identifier = "InternalNavigationCell"
+    
+    let titleLabel = UILabel()
+    let containerView = UIView()
+    
+    override var frame: CGRect {
+        didSet {
+            containerView.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.height)
+            titleLabel.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.height)
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        containerView.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.height)
+        titleLabel.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.height)
+    }
+    
+    fileprivate func customInit() {
+        titleLabel.font = DefaultStyleAttribute.cellTitleLabelFont
+        titleLabel.textAlignment = .center
+        
+        addSubview(containerView)
+        containerView.addSubview(titleLabel)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        customInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        customInit()
     }
 }
 
@@ -89,129 +269,5 @@ class InternalContentView: UIScrollView, UIGestureRecognizerDelegate {
             return true
         }
         return !(contentOffset.x < 1e-6 && panGestureRecognizer.velocity(in: self).x > 1e-6)
-    }
-}
-
-class InternalNavigationView: UIView, UITableViewDelegate, UITableViewDataSource {
-    var currentIndex: Int = 0 {
-        didSet {
-        }
-    }
-    
-    var heightForView = 44 {
-        didSet {
-            
-        }
-    }
-    
-    var titleTexts = [String]()
-    
-    fileprivate let tableView = UITableView()
-    
-    fileprivate func customInit() {
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.showsHorizontalScrollIndicator = false
-        tableView.showsVerticalScrollIndicator = false
-        tableView.allowsMultipleSelection = false
-        tableView.allowsSelectionDuringEditing = false
-        tableView.allowsMultipleSelectionDuringEditing = false
-        tableView.backgroundColor = UIColor.clear
-        tableView.separatorStyle = .none
-        tableView.layer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-        tableView.transform = CGAffineTransform(rotationAngle: CGFloat(M_PI/2))
-        tableView.isScrollEnabled = false
-        tableView.register(InternalNavigationCell.self, forCellReuseIdentifier: InternalNavigationCell.identifier)
-        
-        addSubview(tableView)
-    }
-    
-    override var frame: CGRect {
-        didSet {
-            tableView.frame = CGRect(x: 0, y: 0, width: frame.height, height: frame.width)
-        }
-    }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        customInit()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        customInit()
-    }
-    
-    func scroll(to index: Int, needAnimate animated: Bool, progressive progressiveHandler: ()->()) {
-        
-    }
-    
-    
-    // MARK: - Table View Delegate
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return tableView.bounds.height / CGFloat(titleTexts.count)
-    }
-    
-    // MARK: - Table View Datasource
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return titleTexts.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: InternalNavigationCell.identifier, for: indexPath) as! InternalNavigationCell
-        cell.titleLabel.text = titleTexts[indexPath.row]
-        return cell
-    }
-}
-
-class InternalNavigationCell: UITableViewCell {
-    static let identifier = "InternalNavigationCell"
-    
-    let titleLabel = UILabel()
-    // make sure all your custom views are added to containerView
-    let containerView = UIView()
-    
-    override var frame: CGRect {
-        didSet {
-            containerView.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.height)
-            titleLabel.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.height)
-        }
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        containerView.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.height)
-        titleLabel.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.height)
-    }
-    
-    fileprivate func customInit() {
-        titleLabel.textColor = UIColor.green
-        titleLabel.font = UIFont.systemFont(ofSize: 15)
-        
-        addSubview(containerView)
-        containerView.addSubview(titleLabel)
-//        containerView.layer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-//        containerView.transform = CGAffineTransform(rotationAngle: CGFloat(-M_PI/2))
-    }
-    
-    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        customInit()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        customInit()
     }
 }
--- NetEaseCloudMusic/FindMusic/RecommendViewController.swift
+++ NetEaseCloudMusic/FindMusic/RecommendViewController.swift
@@ -153,19 +153,14 @@ extension RecommendViewController:  UICollectionViewDelegateFlowLayout, UICollec
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let vc = SliderViewController()
         let vc1 = UIViewController()
-        vc1.view.backgroundColor = UIColor.blue
+        vc1.view.backgroundColor = UIColor.lightGray
         let vc2 = UIViewController()
         vc2.view.backgroundColor = UIColor.brown
         let vc3 = UIViewController()
-        vc3.view.backgroundColor = UIColor.red
+        vc3.view.backgroundColor = UIColor.purple
         
         vc.contentViewControllers = [vc1, vc2, vc3]
-        
-        
-        let button = UIButton.init(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
-        button.backgroundColor = UIColor.black
-        button.addTarget(self, action: #selector(test), for: .touchUpInside)
-        vc1.view.addSubview(button)
+        vc.titleTexts = ["One", "Two", "Thre"]
         
         self.navigationController?.pushViewController(vc, animated: true)
     }
